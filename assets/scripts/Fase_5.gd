dfa185202409e837d4b4a2d878c7a9ab39e392ae
Pedro Brown <pedrobrown.br@gmail.com>
2024-10-23 23:00:36 -0300

    Fase 5: estrada completa

--- assets/scripts/Fase_5.gd
+++ assets/scripts/Fase_5.gd
@@ -4,6 +4,15 @@ var C = ["242347", "b36b00", "8c1c1c", "042407", "f5f54c", "63b0a6", "dbdbdb", "
 var color
 
 var scaling = 0.05
+var player_speed
+onready var mini_player = $CanvasLayer/minimap/player
+onready var mini_player_speed
+var mini_player_dir = "up"
+onready var mini_target = $CanvasLayer/minimap/target
+var mini_target_speed = 0.1
+var mini_target_dir = "up"
+
+var tracking_target = true
 
 func _sort_color():
 	randomize()
@@ -46,21 +55,7 @@ func _color_build(build):
 			$background/buildings/Left/building8/color.modulate = color
 
 
-
-func _ready():
-	Global.fase = "Fase_5"
-	_color_build(0)
-	$background/road.play("straight")
-	$background/AnimationPlayerLeft.play("straight")
-	$background/AnimationPlayerLeft.play("straight")
-
-
-
-func _physics_process(delta):
-	$background/road.speed_scale = $Player_motorcycle.foward/10
-	$background/AnimationPlayerLeft.playback_speed = $Player_motorcycle.foward / 10
-	$background/AnimationPlayerRight.playback_speed = $Player_motorcycle.foward / 10
-	
+func _scale_vehicles():
 	for area in $Layers/distance_0.get_overlapping_areas():
 		var vehicle = area.get_parent()
 		if vehicle.scale < Vector2(2, 2):
@@ -100,19 +95,118 @@ func _physics_process(delta):
 			vehicle.scale -= Vector2(scaling, scaling)
 		elif vehicle.scale < Vector2(0.5, 0.5):
 			vehicle.scale += Vector2(scaling, scaling)
+
+
+func _set_dir(new_dir, old_dir):
+	if new_dir == "right":
+		match old_dir:
+			"up":
+				old_dir = "right"
+			"right":
+				old_dir = "down"
+			"down":
+				old_dir = "left"
+			"left":
+				old_dir = "up"
+	elif new_dir == "left":
+		match old_dir:
+			"up":
+				old_dir = "left"
+			"left":
+				old_dir = "down"
+			"down":
+				old_dir = "right"
+			"right":
+				old_dir = "up"
+	return old_dir
+
+
+
+func _ready():
+	Global.fase = "Fase_5"
+	_color_build(0)
+	$background/road.play("straight")
+	$background/AnimationPlayerLeft.play("straight")
+	$background/AnimationPlayerLeft.play("straight")
+
+
+
+func _physics_process(delta):
+	if Global.dead == true:
+		$AnimationPlayer.play("game_over")
+	_scale_vehicles()
+	player_speed = $Player_motorcycle.foward
+	
+	$background/road.speed_scale = player_speed / 10
+	$background/AnimationPlayerLeft.playback_speed = player_speed / 10
+	$background/AnimationPlayerRight.playback_speed = player_speed / 10
+	
 	
 	
 	if $background/AnimationPlayerLeft.current_animation == "turn_right":
-		$Player_motorcycle.position.x -= 5
+		if player_speed == 100:
+			$Player_motorcycle.position.x -= 6
+		elif player_speed < 100 && player_speed >= 75:
+			$Player_motorcycle.position.x -= 5
+		elif player_speed < 75 && player_speed >= 50:
+			$Player_motorcycle.position.x -= 4
+		elif player_speed < 50 && player_speed >=25:
+			$Player_motorcycle.position.x -= 3
+		elif player_speed < 25 && player_speed > 0:
+			$Player_motorcycle.position.x -= 2
+	if $background/AnimationPlayerLeft.current_animation == "turn_left":
+		if player_speed == 100:
+			$Player_motorcycle.position.x += 6
+		elif player_speed < 100 && player_speed >= 75:
+			$Player_motorcycle.position.x += 5
+		elif player_speed < 75 && player_speed >= 50:
+			$Player_motorcycle.position.x += 4
+		elif player_speed < 50 && player_speed >=25:
+			$Player_motorcycle.position.x += 3
+		elif player_speed < 25 && player_speed > 0:
+			$Player_motorcycle.position.x += 2
 	
-	if Input.is_action_just_pressed("ui_right"):
-		$background/road.play("turn_right")
-		$background/AnimationPlayerLeft.play("turn_right")
-		$background/AnimationPlayerRight.play("turn_right")
+	
+	
+	if player_speed == 100:
+		mini_player_speed = 0.1
+	elif player_speed < 100 && player_speed >= 75:
+		mini_player_speed = 0.08
+	elif player_speed < 75 && player_speed >= 50:
+		mini_player_speed = 0.06
+	elif player_speed < 50 && player_speed >=25:
+		mini_player_speed = 0.04
+	elif player_speed < 25 && player_speed > 0:
+		mini_player_speed = 0.02
+	else:
+		mini_player_speed = 0
+	
+	
+	
+	match mini_player_dir:
+		"up":
+			mini_player.position.y -= mini_player_speed
+		"right":
+			mini_player.position.x += mini_player_speed
+		"down":
+			mini_player.position.y += mini_player_speed
+		"left":
+			mini_player.position.x -= mini_player_speed
+	
+	match mini_target_dir:
+		"up":
+			mini_target.position.y -= mini_target_speed
+		"right":
+			mini_target.position.x += mini_target_speed
+		"down":
+			mini_target.position.y += mini_target_speed
+		"left":
+			mini_target.position.x -= mini_target_speed
+	
 
 
 func _on_AnimationPlayerLeft_animation_finished(anim_name):
-	if anim_name == "turn_right":
+	if anim_name == "turn_right" || anim_name == "turn_left":
 		$background/road.play("straight")
 		$background/AnimationPlayerLeft.play("straight")
 		$background/AnimationPlayerRight.play("straight")
@@ -121,8 +215,20 @@ func _on_AnimationPlayerLeft_animation_finished(anim_name):
 
 func _on_Area2D_side_walk_body_entered(body):
 	if body.name == "Player_motorcycle":
-		Global.life = 0
-		$AnimationPlayer.play("game_over")
+		if player_speed == 100:
+			Global.life -= 70
+		elif player_speed < 100 && player_speed >= 75:
+			Global.life -= 50
+		elif player_speed < 75 && player_speed >= 50:
+			Global.life -= 20
+		elif player_speed < 50 && player_speed >=25:
+			Global.life -= 15
+		elif player_speed < 25 && player_speed > 0:
+			Global.life -= 10
+		
+		$Player_motorcycle.foward = 0
+		if Global.life <= 0:
+			Global.dead = true
 
 
 func _on_AnimationPlayer_animation_finished(anim_name):
@@ -130,3 +236,67 @@ func _on_AnimationPlayer_animation_finished(anim_name):
 		get_tree().change_scene("res://assets/interfaces/game_over.tscn")
 	if anim_name == "loading_out":
 		get_tree().change_scene("res://assets/interfaces/credits.tscn")
+
+
+
+
+func _on_sonar_animation_finished():
+	$CanvasLayer/minimap/player/sonar.play("default")
+
+
+func _on_Timer_sonar_timeout():
+	$CanvasLayer/minimap/player/sonar.play("sonar")
+
+
+func _on_Left_area_entered(area):
+	if area.name == "mini_player":
+		mini_player_dir = _set_dir("left", mini_player_dir)
+		mini_player.rotation_degrees -= 90
+		$background/road.flip_h = true
+		$background/road.play("turn")
+		$background/AnimationPlayerLeft.play("turn_left")
+		$background/AnimationPlayerRight.play("turn_left")
+	if area.name == "mini_target":
+		mini_target_dir = _set_dir("left", mini_target_dir)
+		mini_target.rotation_degrees -= 90
+
+
+func _on_Right_area_entered(area):
+	if area.name == "mini_player":
+		mini_player_dir = _set_dir("right", mini_player_dir)
+		mini_player.rotation_degrees += 90
+		$background/road.flip_h = false
+		$background/road.play("turn")
+		$background/AnimationPlayerLeft.play("turn_right")
+		$background/AnimationPlayerRight.play("turn_right")
+	if area.name == "mini_target":
+		mini_target_dir = _set_dir("right", mini_target_dir)
+		mini_target.rotation_degrees += 90
+
+
+func _on_Finish_area_entered(area):
+	if area.name == "mini_player":
+		$AnimationPlayer.play("loading_out")
+	if area.name == "mini_target":
+		mini_target_speed = 0
+
+
+func _on_Area2D_sonar_area_entered(area):
+	if area.name == "mini_target":
+		tracking_target = true
+		mini_target.visible = true
+
+
+
+func _on_Area2D_sonar_area_exited(area):
+	if area.name == "mini_target":
+		tracking_target = false
+		mini_target.visible = false
+		$CanvasLayer/minimap/player/Timer_last_chance.start()
+
+
+func _on_Timer_last_chance_timeout():
+	if tracking_target == false:
+		$AnimationPlayer.play("game_over")
+
+
--- assets/scripts/Player_motorcycle.gd
+++ assets/scripts/Player_motorcycle.gd
@@ -18,79 +18,80 @@ func _ready():
 
 
 func _physics_process(delta):
-	if Input.is_action_pressed("ui_up"):
-		foward += speed
-		if foward > max_speed:
-			foward = max_speed
-	elif Input.is_action_pressed("ui_down"):
-		foward -= speed
-		if foward < 0:
-			foward = 0
-	
-	$motorcycle/tire_back.speed_scale = foward/10
-	$motorcycle/tire_front.speed_scale = foward/10
-	
-	if foward == 100:
-		speed = 5
-	elif foward < 100 && foward >= 75:
-		speed = 4
-	elif foward < 75 && foward >= 50:
-		speed = 3
-	elif foward < 50 && foward >=25:
-		speed = 2
-	elif foward < 25 && foward > 0:
-		speed = 1
-	else:
-		speed = 0.5
-	
-	
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += speed
-		if velocity.x >= 70:
-			$motorcycle/tire_front.position.x = 63
-			$CollisionBack.disabled = true
-			$CollisionSide.disabled = false
-			$CollisionSide.position.x = -7
-			$CollisionSide.rotation_degrees = 57
+	if Global.life > 0:
+		if Input.is_action_pressed("ui_up"):
+			foward += speed
+			if foward > max_speed:
+				foward = max_speed
+		elif Input.is_action_pressed("ui_down"):
+			foward -= speed
+			if foward < 0:
+				foward = 0
+		
+		$motorcycle/tire_back.speed_scale = foward/10
+		$motorcycle/tire_front.speed_scale = foward/10
+		
+		if foward == 100:
+			speed = 5
+		elif foward < 100 && foward >= 75:
+			speed = 4
+		elif foward < 75 && foward >= 50:
+			speed = 3
+		elif foward < 50 && foward >=25:
+			speed = 2
+		elif foward < 25 && foward > 0:
+			speed = 1
+		else:
+			speed = 0.5
+		
+		
+		if Input.is_action_pressed("ui_right"):
+			velocity.x += speed
+			if velocity.x >= 70:
+				$motorcycle/tire_front.position.x = 63
+				$CollisionBack.disabled = true
+				$CollisionSide.disabled = false
+				$CollisionSide.position.x = -7
+				$CollisionSide.rotation_degrees = 57
+				for i in sprites.size():
+					sprites[i].play("side")
+					sprites[i].flip_h = false
+		elif Input.is_action_pressed("ui_left"):
+			velocity.x -= speed
+			if velocity.x <= -70:
+				$motorcycle/tire_front.position.x = -63
+				$CollisionBack.disabled = true
+				$CollisionSide.disabled = false
+				$CollisionSide.position.x = 7
+				$CollisionSide.rotation_degrees = -57
+				for i in sprites.size():
+					sprites[i].play("side")
+					sprites[i].flip_h = true
+		else:
+			if velocity.x > 0:
+				velocity.x -= 4
+			elif velocity.x < 0:
+				velocity.x += 4
+			$CollisionBack.disabled = false
+			$CollisionSide.disabled = true
 			for i in sprites.size():
-				sprites[i].play("side")
-				sprites[i].flip_h = false
-	elif Input.is_action_pressed("ui_left"):
-		velocity.x -= speed
-		if velocity.x <= -70:
-			$motorcycle/tire_front.position.x = -63
-			$CollisionBack.disabled = true
-			$CollisionSide.disabled = false
-			$CollisionSide.position.x = 7
-			$CollisionSide.rotation_degrees = -57
-			for i in sprites.size():
-				sprites[i].play("side")
-				sprites[i].flip_h = true
-	else:
+				sprites[i].play("back")
+		
 		if velocity.x > 0:
-			velocity.x -= 4
+			if velocity.x > max_speed:
+				velocity.x = max_speed
 		elif velocity.x < 0:
-			velocity.x += 4
-		$CollisionBack.disabled = false
-		$CollisionSide.disabled = true
-		for i in sprites.size():
-			sprites[i].play("back")
-	
-	if velocity.x > 0:
-		if velocity.x > max_speed:
-			velocity.x = max_speed
-	elif velocity.x < 0:
-		if velocity.x < -max_speed:
-			velocity.x = -max_speed
-	
-	
-	if velocity.y > 0:
-		if velocity.y > max_speed:
-			velocity.y = max_speed
-	elif velocity.x < 0:
-		if velocity.y < -max_speed:
-			velocity.y = -max_speed
-	
-	
-	#velocity.normalized()
-	move_and_slide(velocity * speed)
+			if velocity.x < -max_speed:
+				velocity.x = -max_speed
+		
+		
+		if velocity.y > 0:
+			if velocity.y > max_speed:
+				velocity.y = max_speed
+		elif velocity.x < 0:
+			if velocity.y < -max_speed:
+				velocity.y = -max_speed
+		
+		
+		#velocity.normalized()
+		move_and_slide(velocity * speed)
