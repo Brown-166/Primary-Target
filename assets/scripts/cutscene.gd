476e60624513ecde50479b81a330f46a3167c5f5
Pedro Brown <pedrobrown.br@gmail.com>
2025-01-11 20:26:22 -0300

    cutscenes flip update

--- assets/scripts/cutscene.gd
+++ assets/scripts/cutscene.gd
@@ -3,101 +3,14 @@ extends Node2D
 var skip = false
 
 export var enemy_ar_flip : bool
-onready var enemy_ar_parts = [
-	$Enemy_AR/Upper_Right/Head, $Enemy_AR/Upper_Right/Chest, 
-	$Enemy_AR/Upper_Right/R_Arm, $Enemy_AR/Upper_Right/R_Forearm, $Enemy_AR/Upper_Right/R_Forearm/R_Hand, 
-	$Enemy_AR/Upper_Left/L_Arm, $Enemy_AR/Upper_Left/L_Forearm, $Enemy_AR/Upper_Left/L_Forearm/L_Hand, 
-	$Enemy_AR/Lower/R_Thigh, $Enemy_AR/Lower/R_Leg, $Enemy_AR/Lower/R_Leg/R_Foot,
-	$Enemy_AR/Lower/L_Thigh, $Enemy_AR/Lower/L_Leg, $Enemy_AR/Lower/L_Leg/L_Foot,
-	$Enemy_AR/Upper_Right/Weapon_Set, $Enemy_AR/Upper_Right/Weapon_Set/Weapon, 
-	$Enemy_AR/Upper_Right/Weapon_Set/Reloader]
-onready var enemy_ar_sprites = [
-	$Enemy_AR/Upper_Right/Head/Sprite, $Enemy_AR/Upper_Right/Chest/Sprite, 
-	$Enemy_AR/Upper_Right/R_Arm/Sprite, $Enemy_AR/Upper_Right/R_Forearm/Sprite, $Enemy_AR/Upper_Right/R_Forearm/R_Hand/Sprite, 
-	$Enemy_AR/Upper_Left/L_Arm/Sprite, $Enemy_AR/Upper_Left/L_Forearm/Sprite, $Enemy_AR/Upper_Left/L_Forearm/L_Hand/Sprite, 
-	$Enemy_AR/Lower/R_Thigh/Sprite, $Enemy_AR/Lower/R_Leg/Sprite, $Enemy_AR/Lower/R_Leg/R_Foot/Sprite,
-	$Enemy_AR/Lower/L_Thigh/Sprite, $Enemy_AR/Lower/L_Leg/Sprite, $Enemy_AR/Lower/L_Leg/L_Foot/Sprite,
-	$Enemy_AR/Upper_Right/Weapon_Set/Weapon/Sprite, 
-	$Enemy_AR/Upper_Right/Weapon_Set/Reloader/Sprite]
-var enemy_ar_or_position_x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 
-	14, 15, 16]
 
 export var player_flip : bool
-onready var player_parts = [
-	$Player/Upper_Right, $Player/Upper_Left, $Player/Lower,
-	$Player/Upper_Right/Head, $Player/Upper_Right/Chest,
-	$Player/Upper_Right/R_Arm, $Player/Upper_Right/R_Arm/R_Forearm, $Player/Upper_Right/R_Arm/R_Forearm/R_Hand,
-	$Player/Upper_Left/L_Arm, $Player/Upper_Left/L_Arm/L_Forearm, $Player/Upper_Left/L_Arm/L_Forearm/L_Hand,
-	$Player/Lower/R_Thigh, $Player/Lower/R_Thigh/R_Leg, $Player/Lower/R_Thigh/R_Leg/R_Foot,
-	$Player/Lower/L_Thigh, $Player/Lower/L_Thigh/L_Leg, $Player/Lower/L_Thigh/L_Leg/L_Foot,
-	$Player/Upper_Right/Katana]
-onready var player_sprites = [
-	$Player/Upper_Right/Head, $Player/Upper_Right/Chest,
-	$Player/Upper_Right/R_Arm, $Player/Upper_Right/R_Arm/R_Forearm, $Player/Upper_Right/R_Arm/R_Forearm/R_Hand,
-	$Player/Upper_Left/L_Arm, $Player/Upper_Left/L_Arm/L_Forearm, $Player/Upper_Left/L_Arm/L_Forearm/L_Hand,
-	$Player/Lower/R_Thigh, $Player/Lower/R_Thigh/R_Leg, $Player/Lower/R_Thigh/R_Leg/R_Foot,
-	$Player/Lower/L_Thigh, $Player/Lower/L_Thigh/L_Leg, $Player/Lower/L_Thigh/L_Leg/L_Foot,
-	$Player/Upper_Right/Katana/Blade, $Player/Upper_Right/Katana/Hilt]
-var player_or_position_x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 
-	14, 15, 16, 17]
 
 export var hades_flip : bool
-onready var hades_parts = [
-	$Hades/Upper_Right, $Hades/Upper_Left, $Hades/Lower,
-	$Hades/Upper_Right/Head, $Hades/Upper_Right/Chest,
-	$Hades/Upper_Right/R_Arm, $Hades/Upper_Right/R_Arm/R_Forearm, $Hades/Upper_Right/R_Arm/R_Forearm/R_Hand,
-	$Hades/Upper_Left/L_Arm, $Hades/Upper_Left/L_Arm/L_Forearm, $Hades/Upper_Left/L_Arm/L_Forearm/L_Hand,
-	$Hades/Lower/R_Thigh, $Hades/Lower/R_Thigh/R_Leg, $Hades/Lower/R_Thigh/R_Leg/R_Foot,
-	$Hades/Lower/L_Thigh, $Hades/Lower/L_Thigh/L_Leg, $Hades/Lower/L_Thigh/L_Leg/L_Foot]
-onready var hades_sprites = [
-	$Hades/Upper_Right/Head, $Hades/Upper_Right/Chest,
-	$Hades/Upper_Right/R_Arm, $Hades/Upper_Right/R_Arm/R_Forearm, $Hades/Upper_Right/R_Arm/R_Forearm/R_Hand,
-	$Hades/Upper_Left/L_Arm, $Hades/Upper_Left/L_Arm/L_Forearm, $Hades/Upper_Left/L_Arm/L_Forearm/L_Hand,
-	$Hades/Lower/R_Thigh, $Hades/Lower/R_Thigh/R_Leg, $Hades/Lower/R_Thigh/R_Leg/R_Foot,
-	$Hades/Lower/L_Thigh, $Hades/Lower/L_Thigh/L_Leg, $Hades/Lower/L_Thigh/L_Leg/L_Foot]
-var hades_or_position_x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
 
 export var boss_blue_flip : bool
-onready var boss_blue_parts = [
-	$Boss_Blue/Upper_Right, $Boss_Blue/Upper_Left, $Boss_Blue/Lower,
-	$Boss_Blue/Upper_Right/Head, $Boss_Blue/Upper_Right/Chest,
-	$Boss_Blue/Upper_Right/R_Arm, $Boss_Blue/Upper_Right/R_Arm/R_Forearm, $Boss_Blue/Upper_Right/R_Arm/R_Forearm/R_Hand,
-	$Boss_Blue/Upper_Left/L_Arm, $Boss_Blue/Upper_Left/L_Arm/L_Forearm, $Boss_Blue/Upper_Left/L_Arm/L_Forearm/L_Hand,
-	$Boss_Blue/Lower/R_Thigh, $Boss_Blue/Lower/R_Thigh/R_Leg, $Boss_Blue/Lower/R_Thigh/R_Leg/R_Foot,
-	$Boss_Blue/Lower/L_Thigh, $Boss_Blue/Lower/L_Thigh/L_Leg, $Boss_Blue/Lower/L_Thigh/L_Leg/L_Foot,
-	$Boss_Blue/Upper_Right/Katar, $Boss_Blue/Upper_Left/Katar]
-onready var boss_blue_sprites = [
-	$Boss_Blue/Upper_Right/Head/Sprite, $Boss_Blue/Upper_Right/Chest/Sprite,
-	$Boss_Blue/Upper_Right/R_Arm/Sprite, $Boss_Blue/Upper_Right/R_Arm/R_Forearm/Sprite, $Boss_Blue/Upper_Right/R_Arm/R_Forearm/R_Hand/Sprite,
-	$Boss_Blue/Upper_Left/L_Arm/Sprite, $Boss_Blue/Upper_Left/L_Arm/L_Forearm/Sprite, $Boss_Blue/Upper_Left/L_Arm/L_Forearm/L_Hand/Sprite,
-	$Boss_Blue/Lower/R_Thigh/Sprite, $Boss_Blue/Lower/R_Thigh/R_Leg/Sprite, $Boss_Blue/Lower/R_Thigh/R_Leg/R_Foot/Sprite,
-	$Boss_Blue/Lower/L_Thigh/Sprite, $Boss_Blue/Lower/L_Thigh/L_Leg/Sprite, $Boss_Blue/Lower/L_Thigh/L_Leg/L_Foot/Sprite,
-	$Boss_Blue/Upper_Right/Katar/Hilt, $Boss_Blue/Upper_Right/Katar/Blade,
-	$Boss_Blue/Upper_Left/Katar/Hilt, $Boss_Blue/Upper_Left/Katar/Blade]
-var boss_blue_or_position_x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 
-	14, 15, 16, 17, 18, 19]
 
 export var boss_red_flip : bool
-onready var boss_red_parts = [
-	$Boss_Red/Upper_Right, $Boss_Red/Upper_Left, $Boss_Red/Lower,
-	$Boss_Red/Upper_Right/Head, $Boss_Red/Upper_Right/Chest,
-	$Boss_Red/Upper_Right/R_Arm, $Boss_Red/Upper_Right/R_Arm/R_Forearm, $Boss_Red/Upper_Right/R_Arm/R_Forearm/R_Hand,
-	$Boss_Red/Upper_Left/L_Arm, $Boss_Red/Upper_Left/L_Arm/L_Forearm, $Boss_Red/Upper_Left/L_Arm/L_Forearm/L_Hand,
-	$Boss_Red/Lower/R_Thigh, $Boss_Red/Lower/R_Thigh/R_Leg, $Boss_Red/Lower/R_Thigh/R_Leg/R_Foot,
-	$Boss_Red/Lower/L_Thigh, $Boss_Red/Lower/L_Thigh/L_Leg, $Boss_Red/Lower/L_Thigh/L_Leg/L_Foot,
-	$Boss_Red/Upper_Right/Hammer]
-onready var boss_red_sprites = [
-	$Boss_Red/Upper_Right/Head/Sprite, $Boss_Red/Upper_Right/Chest/Sprite,
-	$Boss_Red/Upper_Right/R_Arm/Sprite, $Boss_Red/Upper_Right/R_Arm/R_Forearm/Sprite, $Boss_Red/Upper_Right/R_Arm/R_Forearm/R_Hand/Sprite,
-	$Boss_Red/Upper_Left/L_Arm/Sprite, $Boss_Red/Upper_Left/L_Arm/L_Forearm/Sprite, $Boss_Red/Upper_Left/L_Arm/L_Forearm/L_Hand/Sprite,
-	$Boss_Red/Lower/R_Thigh/Sprite, $Boss_Red/Lower/R_Thigh/R_Leg/Sprite, $Boss_Red/Lower/R_Thigh/R_Leg/R_Foot/Sprite,
-	$Boss_Red/Lower/L_Thigh/Sprite, $Boss_Red/Lower/L_Thigh/L_Leg/Sprite, $Boss_Red/Lower/L_Thigh/L_Leg/L_Foot/Sprite,
-	$Boss_Red/Upper_Right/Hammer/Hilt, $Boss_Red/Upper_Right/Hammer/Head]
-var boss_red_or_position_x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 
-	14, 15, 16, 17, 18]
-
-
-
 
 
 onready var background_1 = [$background_1/city/Sprite0, $background_1/city/Sprite, 
@@ -172,30 +85,46 @@ func _boss_red_flip():
 		boss_red_flip = false
 
 
+func _flip():
+	if get_node("Enemy_AR"):
+		if enemy_ar_flip == false:
+			get_node("Enemy_AR").scale.x = 2
+		else:
+			get_node("Enemy_AR").scale.x = -2
+	
+	
+	if get_node("Player"):
+		if player_flip == false:
+			get_node("Player").scale.x = 2
+		else:
+			get_node("Player").scale.x = -2
+	
+	
+	if get_node("Hades"):
+		if hades_flip == false:
+			get_node("Hades").scale.x = 2
+		else:
+			get_node("Hades").scale.x = -2
+	
+	
+	if get_node("Boss_Blue"):
+		if boss_blue_flip == false:
+			get_node("Boss_Blue").scale.x = 2
+		else:
+			get_node("Boss_Blue").scale.x = -2
+	
+	
+	if get_node("Boss_Red"):
+		if boss_red_flip == false:
+			get_node("Boss_Red").scale.x = 2
+		else:
+			get_node("Boss_Red").scale.x = -2
+
 
 func _ready():
 	Global.fase = name
 	DB._save_new_game(DB.current_save)
 	
-	if get_node("Enemy_AR"):
-		for i in enemy_ar_parts.size():
-			enemy_ar_or_position_x[i] = enemy_ar_parts[i].position.x
-	
-	if get_node("Player"):
-		for i in player_parts.size():
-			player_or_position_x[i] = player_parts[i].position.x
-	
-	if get_node("Hades"):
-		for i in hades_parts.size():
-			hades_or_position_x[i] = hades_parts[i].position.x
-	
-	if get_node("Boss_Blue"):
-		for i in boss_blue_parts.size():
-			boss_blue_or_position_x[i] = boss_blue_parts[i].position.x
-	
-	if get_node("Boss_Red"):
-		for i in boss_red_parts.size():
-			boss_red_or_position_x[i] = boss_red_parts[i].position.x
 	
 	if Global.fase == "cutscene_3":
 		for i in background_1.size():
@@ -215,6 +144,7 @@ func _ready():
 
 
 func _physics_process(delta):
+	_flip()
 	if Global.fase == "cutscene_3":
 		for i in background_1.size():
 			background_1[i].position.x -= background_1_velocity
@@ -276,70 +206,3 @@ func _on_AnimationPlayer_animation_finished(anim_name):
 				get_tree().change_scene("res://assets/scenes/Fase_6.tscn")
 			"cutscene_7":
 				get_tree().change_scene("res://assets/interfaces/credits.tscn")
-		
-
-
-func _on_Timer_Flip_timeout():
-	if get_node("Enemy_AR"):
-		if enemy_ar_flip == true:
-			for i in enemy_ar_parts.size():
-				if enemy_ar_parts[i].position.x == enemy_ar_or_position_x[i]:
-					enemy_ar_parts[i].position.x *= -1
-					enemy_ar_parts[i].rotation_degrees *= -1
-			for i in enemy_ar_sprites.size():
-				enemy_ar_sprites[i].flip_h = true
-		else:
-			for i in enemy_ar_sprites.size():
-				enemy_ar_sprites[i].flip_h = false
-	
-	
-	if get_node("Player"):
-		if player_flip == true:
-			for i in player_parts.size():
-				if player_parts[i].position.x == player_or_position_x[i]:
-					player_parts[i].position.x *= -1
-					player_parts[i].rotation_degrees *= -1
-			for i in player_sprites.size():
-				player_sprites[i].flip_h = true
-		else:
-			for i in player_sprites.size():
-				player_sprites[i].flip_h = false
-	
-	
-	if get_node("Hades"):
-		if hades_flip == true:
-			for i in hades_parts.size():
-				if hades_parts[i].position.x == hades_or_position_x[i]:
-					hades_parts[i].position.x *= -1
-					hades_parts[i].rotation_degrees *= -1
-			for i in hades_sprites.size():
-				hades_sprites[i].flip_h = true
-		else:
-			for i in hades_sprites.size():
-				hades_sprites[i].flip_h = false
-	
-	
-	if get_node("Boss_Blue"):
-		if boss_blue_flip == true:
-			for i in boss_blue_parts.size():
-				if boss_blue_parts[i].position.x == boss_blue_or_position_x[i]:
-					boss_blue_parts[i].position.x *= -1
-					boss_blue_parts[i].rotation_degrees *= -1
-			for i in boss_blue_sprites.size():
-				boss_blue_sprites[i].flip_h = true
-		else:
-			for i in boss_blue_sprites.size():
-				boss_blue_sprites[i].flip_h = false
-	
-	
-	if get_node("Boss_Red"):
-		if boss_red_flip == true:
-			for i in boss_red_parts.size():
-				if boss_red_parts[i].position.x == boss_red_or_position_x[i]:
-					boss_red_parts[i].position.x *= -1
-					boss_red_parts[i].rotation_degrees *= -1
-			for i in boss_red_sprites.size():
-				boss_red_sprites[i].flip_h = true
-		else:
-			for i in boss_red_sprites.size():
-				boss_red_sprites[i].flip_h = false
